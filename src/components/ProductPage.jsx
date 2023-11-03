a8745ac8227dfd86c43530ab077bbb5d964208b5
Rony Hossain <rooneyhossain@gmail.com>
2023-11-03 21:38:40 +0600

    new page done

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,5 +1,87 @@
+import { AiOutlineSearch } from "react-icons/ai";
+import "./css/product-page.css";
+import ProductCart from "./mincomp/ProductCart";
+
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <section className="product-page">
+      <div className="side-bar">
+        <div className="brands">
+          <p>Brands</p>
+          <div className="input-fuild">
+            <div className="input">
+              <input type="checkbox" id="tenda" name="drone" value="tenda" />
+              <label htmlFor="tenda">
+                tenda <span>(0)</span>
+              </label>
+            </div>
+            <div className="input">
+              <input type="checkbox" id="tplink" name="drone" value="tplink" />
+              <label htmlFor="tplink">
+                tp-link <span>(0)</span>
+              </label>
+            </div>
+            <div className="input">
+              <input type="checkbox" id="netis" name="drone" value="netis" />
+              <label htmlFor="netis">
+                netis <span>(0)</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="price-range">
+          <p>Price range</p>
+
+          <div className="range-input">
+            <input
+              type="range"
+              className="range"
+              id="range"
+              name="range"
+              max="10000"
+            />
+            <label htmlFor="range">10000 tk</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="products">
+        <div className="top-bar">
+          <div className="search-bar">
+            <input type="text" placeholder="Search.." />
+            <AiOutlineSearch className="ai"/>
+          </div>
+        </div>
+        <div className="product">
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
+        </div>
+        <div className="pagination">
+          <a href="#">&laquo;</a>
+          <a href="#">1</a>
+          <a href="#" className="active">
+            2
+          </a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+          <a href="#">&raquo;</a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductPage;
---
+++ src/components/mincomp/ProductCart.jsx
@@ -0,0 +1,21 @@
+import "./productcart.css";
+const ProductCart = () => {
+  return (
+    <div className="product-cart">
+      <div className="cart-img">
+        <img src="img/tenda.png" alt="product" />
+      </div>
+      <div className="cart-content">
+        <h1>tenda router</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+          libero vel autem perferendis itaque cumque.
+        </p>
+        <p className="price">price: 3000/- tk</p>
+        <a >Buy now</a>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCart;
